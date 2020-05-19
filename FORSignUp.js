cebda2c1f17848edb9562462d15d646a5a5b57ad
Hlib Makarov <makarovhlib@gmail.com>
2020-05-19 04:17:07 +0300

    Backup

--- FORSignUp.js
+++ FORSignUp.js
@@ -7,20 +7,26 @@ class ForSignUpDown {
         let arr = ["EDTb2ZTMzgUg7Uaw", "8kTrZyFt4GGV4JSv", "2zhxdYYRGyeddLjK","x5kz87zdWXxxHa2r","8j6UhokMavn2w9Z2"];
         let email = ["Test1@gmail.com", "Test2@gmail.com", "Test3@gmail.com","Test4@gmail.com","Test5@gmail.com"];
         let password = ['9:)L"j/)Q$wT~<ie', 'n]%Yo:*a:-pu8S%', 'b}#2<WiGxT)GU;', '[J#~;Ek+^S*Rd$M8', 'FwoM<`y[~Z;{eK5K']
+
+        const usernamefield = $('[id="user[login]-footer"]')
+        const emailfield =  $('[id="user[email]-footer"]')
+        const passfield =  $('[id="user[password]-footer"]')
+        const signupbutton =  $('[class="btn-mktg btn-primary-mktg btn-block mt-n1"]')
     
         for (let i = 0; i < arr.length; i++) {
             browser.setWindowSize(1920,1080)
             browser.url('https://github.com/')
-            $('[id="user[login]-footer"]').scrollIntoView()
-            $('[id="user[login]-footer"]').clearValue()
-            $('[id="user[login]-footer"]').addValue(arr[i])
-            $('[id="user[email]-footer"]').clearValue()
-            $('[id="user[email]-footer"]').addValue(email[i])
-            $('[id="user[password]-footer"]').clearValue()
-            $('[id="user[password]-footer"]').addValue(password[i])
+            usernamefield.scrollIntoView()
+            usernamefield.clearValue()
+            usernamefield.addValue(arr[i])
+            emailfield.clearValue()
+            emailfield.addValue(email[i])
+            passfield.clearValue()
+            passfield.addValue(password[i])
         
-            //$('[class="btn-mktg btn-primary-mktg btn-block mt-n1"]').click()
-            browser.pause(2000)
+            signupbutton.isExisting()
+            signupbutton.click()
+            browser.pause(000)
             }
         })
     }
